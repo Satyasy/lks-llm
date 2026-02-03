1abe74d2688ecc625c0e78aa87e67b49f176abad
Satyasy <satyavpandega@gmail.com>
2026-02-03 11:16:20 +0700

    day 1

--- client-apps/next.config.mjs
+++ client-apps/next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Mengubah menjadi static HTML
-  images: {
-    unoptimized: true, // Wajib untuk static export agar tidak error saat build gambar
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
